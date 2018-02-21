aef0b3202f652f2c687235902f29bb56b90e11c0
progdesigner <me@progdesigner.com>
2018-02-21 19:27:21 +0900

    0.9.1
    - 테스트 소스 추가

--- test/basic-test.js
+++ test/basic-test.js
@@ -1,10 +1,104 @@
 var Macro = require('../');
 
-let macro = new Macro("BASIC-TEST");
+let macro = new Macro("BASIC-TEST", {skipError:true});
+let data = [];
+let reseted = false;
 
-macro.add("Action #1 - Basic", (command, info, error) => {
-
+macro.onChange((macro) => {
+    renderData();
 });
 
+function renderData() {
+    console.clear();
+    data.forEach( (sequence) => {
+        let status = 'Unknown';
+        if (sequence.hasError) {
+            status = 'Error';
+        }
+        else if (sequence.isDone) {
+            status = 'Done';
+        }
+        else if (sequence.isRunning) {
+            status = 'Running' + ' (' + sequence.executeCount + ')';
+        }
+        else {
+            status = 'Ready';
+        }
+        console.log( sequence.tag, status );
+    });
+}
 
-macro.start();
+function reset( autoStart ) {
+
+    console.log( "reset", autoStart );
+
+    macro.add("Action #1 - Basic", (command, info, error) => {
+        data.push( command );
+        renderData();
+        command.complete();
+    });
+
+    macro.add("Action #2 - DelayCompletion", (command, info, error) => {
+        data.push( command );
+        renderData();
+
+        setTimeout(() => {
+            command.complete();
+        }, 500);
+    });
+
+    macro.delay(750);
+
+    macro.add("Action #3 - After Delay", (command, info, error) => {
+        data.push( command );
+        command.complete();
+    });
+
+    macro.add("Action #4 - Retry", (command, info, error) => {
+        if (command.executeCount === 1) {
+            data.push( command );
+            renderData();
+        }
+
+        setTimeout(() => {
+            if (command.executeCount < 3) {
+                command.retry();
+            }
+            else {
+                command.complete();
+            }
+        }, 500);
+    });
+
+    macro.add("Action #5 - Reset", (command, info, error) => {
+        data.push( command );
+        renderData();
+
+        if (reseted === false) {
+            reseted = true;
+            reset(true);
+        }
+
+        command.complete();
+    });
+
+    macro.add("Action #6 - Error", (command, info, error) => {
+        data.push( command );
+        renderData();
+
+        let customError = new Error("error test");
+        command.error(customError);
+        renderData();
+
+        command.complete();
+    });
+
+    if (autoStart === true) {
+        macro.start((error) => {
+            console.log( "finish", error );
+
+        });
+    }
+}
+
+reset(true);
